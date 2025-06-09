f4c44b4d64c6e50101d4169e972aca9dda7e9c64
hossainshofiq <ah731710@gmail.com>
2025-06-09 15:43:02 +0600

    sign up user partially done

--- src/app/actions/auth/SignUpUser.js
+++ src/app/actions/auth/SignUpUser.js
@@ -3,27 +3,25 @@ import bcrypt from "bcrypt";
 import dbConnect, { collectionNamesObject } from "@/library/dbConnect";
 
 export const signUpUser = async (payload) => {
-    // console.log("SignUpUser called with payload:", payload);
+    console.log("SignUpUser called with payload:", payload);
+
     const usersCollection = dbConnect(collectionNamesObject.usersCollection);
 
-    const { name, email, password } = payload;
-    if (!name || !email || !password ) return { success: false };
+    const { email, password } = payload;
+    if (!email || !password) return { success: false };
+
 
     const user = await usersCollection.findOne({ email: payload.email });
     if (!user) {
         const hashedPassword = await bcrypt.hash(password, 10);
-        // const hashedConfirmPassword = await bcrypt.hash(confirmPassword, 10);
         payload.password = hashedPassword;
-        // payload.confirmPassword = hashedConfirmPassword;
+
         const result = await usersCollection.insertOne(payload);
-
-        // const { acknowledged, insertedId } = result;
-        // return { acknowledged, insertedId };
-
-        const { _id} = result;
-        return { _id };
+        const { acknowledged, insertedId } = result;
+        return { acknowledged, insertedId };
     }
 
     return { success: false };
+
 }
 
--- src/app/signup/components/SignUpForm.jsx
+++ src/app/signup/components/SignUpForm.jsx
@@ -1,9 +1,10 @@
 'use client';
-import { signUpUser } from '@/app/actions/auth/SignUpUser';
+import { signUpUser } from '@/app/actions/auth/signUpUser';
 import React from 'react';
-import { FaGoogle } from 'react-icons/fa';
+import { FaFacebook, FaGoogle } from 'react-icons/fa';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
 
 const SignUpForm = () => {
 
@@ -16,28 +17,30 @@ const SignUpForm = () => {
         const password = form.password.value;
         // const confirmPassword = form.confirmPassword.value;
 
-        if (!email || !password) {
-            alert("All fields are required");
-            return;
-        }
+        // console.log({ name, email, password });
+        await signUpUser({ name, email, password })
+
+        // if (!email || !password) {
+        //     alert("All fields are required");
+        //     return;
+        // }
 
         // if (password !== confirmPassword) {
         //     alert("Passwords do not match");
         //     return;
         // }
 
-        await signUpUser({ name, email, password });
-        toast("Sign up successful! Please log in.", {
-            type: "success",
-            position: "top-right",
-            autoClose: 3000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-        })
-        console.log({ name, email, password });
+        // toast("Sign up successful! Please log in.", {
+        //     type: "success",
+        //     position: "top-right",
+        //     autoClose: 3000,
+        //     hideProgressBar: false,
+        //     closeOnClick: true,
+        //     pauseOnHover: true,
+        //     draggable: true,
+        //     progress: undefined,
+        // })
+
 
         // TODO: Send data to your backend API or authentication service
     };
@@ -58,6 +61,7 @@ const SignUpForm = () => {
                     draggable
                     pauseOnHover
                 />
+
                 <form onSubmit={handleSubmit} className="space-y-6">
                     <div>
                         <label className="block text-sm font-medium text-gray-700">
@@ -149,8 +153,12 @@ const SignUpForm = () => {
                                 <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05" />
                                 <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335" />
                             </svg> */}
-                            <FaGoogle></FaGoogle>
+                            <FaGoogle className=''></FaGoogle>
                             Sign up with Google
+                        </button>
+                        <button className='w-full inline-flex items-center gap-2 justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-blue-600 hover:bg-gray-50'>
+                            <FaFacebook className='text-blue-600'></FaFacebook>
+                            Sign up with Facebook
                         </button>
                     </div>
                 </div>
